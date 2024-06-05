7d141074cea8bfe84cefc92cef8b457276b42423
devimarchanda <164175293+devimarchanda@users.noreply.github.com>
2024-06-05 17:21:05 +0700

    Create konek.php

---
+++ konek.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    echo "Selamat datang semoga beruntung";
+    echo "gunakan website ini dengan bijak";
+    
+    ?>
+</body>
+</html>
