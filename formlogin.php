d871ad7403578920b302ef47a5e292401502afc5
devimarchanda <164175293+devimarchanda@users.noreply.github.com>
2024-06-05 17:20:17 +0700

    Create formlogin.php

---
+++ formlogin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Login App</title>
+</head>
+<body>
+    <h1>Login App</h1>
+    <form action="konek.php" method="post">
+        <table border="0">
+            <tr>
+                <td>NIM</td>
+                <td>:</td>
+                <td><input type="text" name="NIM"></td>
+            </tr>
+            <tr>
+            <td>Password</td>
+            <td>:</td>
+            <td><input type="Password" name="Password"></td>
+            </tr>
+
+        </table><br>
+        <input type="submit" value="Kirim">
+    </form>
+</body>
+</html>
